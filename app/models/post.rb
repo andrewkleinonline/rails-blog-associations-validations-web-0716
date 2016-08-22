615a4c5f9da215f1ec51fab9216e66614c2f9697
Andrew Klein <andrew.klein.j@gmail.com>
2016-08-22 10:15:57 -0400

    complete lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ActiveRecord::Base
-  
+  validates :content, presence: true
+  validates :name, presence: true
+
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+  belongs_to :user
+
+  accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes[:name].blank? }
+
+
 end
